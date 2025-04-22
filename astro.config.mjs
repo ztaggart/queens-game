16a01a7db7a6df2600d8f0d31d2a5f4b6b63bd14
Zachary Taggart <ztaggart020701@gmail.com>
2025-04-22 11:25:11 -0400

    deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: "https://ztaggart.github.io",
+  base: "queens-game",
 });
